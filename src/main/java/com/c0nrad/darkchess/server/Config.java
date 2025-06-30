d66a64e3699e0eeaa1daa9106ee1372682365942
13FrancoCaballero <137089422+13FrancoCaballero@users.noreply.github.com>
2025-06-30 20:23:45 -0300

    Update Config.java

--- src/main/java/com/c0nrad/darkchess/server/Config.java
+++ src/main/java/com/c0nrad/darkchess/server/Config.java
@@ -1,15 +1,14 @@
 package com.c0nrad.darkchess.server;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
 
-    public final static String PORT="port";
-    public final static String MONGOURI="mongouri";
+    public final static String PORT = "port";
+    public final static String MONGOURI = "mongouri";
 
     public static Properties properties;
 
@@ -18,5 +17,6 @@ public class Config {
 
         try (InputStream input = new FileInputStream("./config/config.properties")) {
             properties.load(input);
+        }
     }
 }
